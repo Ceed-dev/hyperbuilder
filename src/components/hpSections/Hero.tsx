8d7ca55537480c1ed93323d51da6d3c44db6916b
shungo0222 <kimura.shungo@gmail.com>
2025-05-27 12:23:28 +0900

    Refine Hero section and NavBar component sizes, margins, and responsive adjustments

--- src/components/hpSections/Hero.tsx
+++ src/components/hpSections/Hero.tsx
@@ -7,13 +7,15 @@ export default function Hero() {
   return (
     <section
       className="
-        w-full h-screen flex flex-col items-center justify-center rounded-3xl text-[#072723] text-center font-bold relative
+        w-full min-h-[calc(100vh-1.5rem)] flex flex-col items-center justify-center rounded-3xl text-[#072723] text-center relative
         bg-[url('/hp/hero/bg-mobile.svg')] bg-cover bg-center
         md:bg-[url('/hp/hero/bg-desktop.svg')]"
     >
       {/* Central Hero Image */}
       <div className="flex-grow flex items-center justify-center">
-        <div className="relative w-full max-w-[1200px] mb-60 md:mb-40">
+        <div className="relative w-full
+          max-w-[280px] md:max-w-[500px] 2xl:max-w-[1200px]
+          mb-60 md:mb-40">
           <Image
             src="/hp/hero/logo-hyperliquid.svg"
             alt="Hero Object"
@@ -25,22 +27,22 @@ export default function Hero() {
       </div>
 
       {/* Bottom Fixed Content */}
-      <div className="w-full px-5 pb-10 absolute bottom-0 flex flex-col items-center">
+      <div className="w-full px-5 pb-10 md:pb-20 absolute bottom-0 flex flex-col items-center">
         {/* Headline */}
-        <h1 className="flex flex-wrap items-center justify-center text-3xl md:text-4xl font-semibold mb-4">
+        <h1 className="flex flex-wrap items-center justify-center text-3xl md:text-4xl mb-4">
           <span>A home of</span>
           <Image
             src="/hp/hero/text-hyperliquid.svg"
             alt="Hyperliquid Icon"
             width={200}
             height={40}
-            className="mx-4"
+            className="mx-4 relative top-[4px]"
           />
           <span>builders</span>
         </h1>
 
         {/* Description */}
-        <p className="text-sm sm:text-base mb-4 text-center">
+        <p className="text-xs md:text-sm mb-4 text-center font-semibold">
           Hyperbuilder is an onboarding gateway for Hyperliquid enthusiasts.
           <br />
           Whether you’re a builder, content creator, marketer, designer,
