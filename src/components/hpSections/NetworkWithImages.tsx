98ea0a17704a62756e1a901c878f1a6acf8f71af
shungo0222 <kimura.shungo@gmail.com>
2025-05-28 11:31:57 +0900

    Add country flags with individual hyperlinks to Network section

--- src/components/hpSections/NetworkWithImages.tsx
+++ src/components/hpSections/NetworkWithImages.tsx
@@ -86,8 +86,8 @@ export default function NetworkWithImages() {
         </div>
 
         {/* Card 4 */}
-        <div className="order-4 md:order-3">
-          {/* Mobile Image */}
+        <div className="order-4 md:order-3 relative">
+          {/* Background Image (mobile) */}
           <Image
             src="/hp/network/cards/4-mobile.svg"
             alt="Globe Background"
@@ -95,7 +95,7 @@ export default function NetworkWithImages() {
             height={100}
             className="w-full h-full object-contain block md:hidden"
           />
-          {/* Desktop Image */}
+          {/* Background Image (desktop) */}
           <Image
             src="/hp/network/cards/4-desktop.svg"
             alt="Globe Background"
@@ -103,6 +103,66 @@ export default function NetworkWithImages() {
             height={100}
             className="w-full h-full object-contain hidden md:block"
           />
+
+          {/* Overlay: Country Flags with Links */}
+          <div className="absolute left-[5%] bottom-[7%] xl:bottom-[10%] 3xl:bottom-[15%] flex gap-3 xl:gap-8 3xl:gap-10">
+            {/* Vietnam */}
+            <a
+              href="https://t.me/HyperliquidViet_Nam"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition transform hover:-translate-y-1"
+            >
+              <Image
+                src="/hp/network/flag-vietnam.svg"
+                alt="Vietnam Flag"
+                width={50}
+                height={50}
+                className="w-[66px] h-[66px] md:w-[45px] md:h-[45px] xl:w-[80px] xl:h-[80px] 2xl:w-[100px] 2xl:h-[100px] object-contain"
+              />
+              <p className="text-black text-[12px] xl:text-[14px] font-[500] text-center xl:mt-2">
+                Vietnam
+              </p>
+            </a>
+
+            {/* Japan */}
+            <a
+              href="https://t.me/hyperliquid_jp_group"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition transform hover:-translate-y-1"
+            >
+              <Image
+                src="/hp/network/flag-japan.svg"
+                alt="Japan Flag"
+                width={50}
+                height={50}
+                className="w-[66px] h-[66px] md:w-[45px] md:h-[45px] xl:w-[80px] xl:h-[80px] 2xl:w-[100px] 2xl:h-[100px] object-contain"
+              />
+              <p className="text-black text-[12px] xl:text-[14px] font-[500] text-center xl:mt-2">
+                Japan
+              </p>
+            </a>
+
+            {/* China */}
+            <a
+              href="https://t.me/hyperliquid_china_group"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition transform hover:-translate-y-1"
+            >
+              <Image
+                src="/hp/network/flag-china.svg"
+                alt="China Flag"
+                width={50}
+                height={50}
+                className="w-[66px] h-[66px] md:w-[45px] md:h-[45px] xl:w-[80px] xl:h-[80px] 2xl:w-[100px] 2xl:h-[100px] object-contain"
+              />
+              <p className="text-black text-[12px] xl:text-[14px] font-[500] text-center xl:mt-2">
+                China
+              </p>
+            </a>
+          </div>
         </div>
       </div>
     </section>
